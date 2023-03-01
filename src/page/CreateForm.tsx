ee256d198419d7aa56c0ebe680e796d798a3f480
Taffymark1 <pjhyun314@gmail.com>
2023-03-01 15:55:29 +0900

    formData로 변경

--- src/page/CreateForm.tsx
+++ src/page/CreateForm.tsx
@@ -2,101 +2,107 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
+import { getCookie } from '../util/cookie'
 import styled from 'styled-components'
 
 function CreateForm() {
   const navigate = useNavigate()
   const dispatch = useDispatch()
-  const [isImage, setImage] = useState({
-    image_file: "",
-    preview_URL: "",
-  });
-
   let inputRef;
-
+  let token = getCookie('accessToken') // 쿠키에저장
+  const [isImage, setImage] = useState('');
+  const [previewImg, setPreviewImg ] = useState('');
   const [data, setData] = useState({
-    Title: '',
-    Mbti: '',
-    Material: '',
-    Content: '',
+    title: '',
+    mbti: '',
+    material: '',
+    content: '',
+    imageUrl: '',
   })
 
   const onClickHandler = async(e: any) => {
     e.preventDefault();
 
-    // console.log(isImage.image_file);
-    // console.log(data.Title);
-    // console.log(data.Mbti);
-    // console.log(data.Material);
-    // console.log(data.Content);
+    console.log(isImage);
+    console.log(data.title);
+    console.log(data.mbti);
+    console.log(data.material);
+    console.log(data.content);
+    console.log(data.imageUrl);
 
-    if (isImage.image_file === '' ) {
+    if (isImage === '' ) {
       return alert('이미지를 넣어주세요.');
     }
-    if (data.Title.trim() === '') {
+    if (data.title.trim() === '') {
       return alert('제목을 입력해주세요.')
     }
-    if (data.Mbti.trim() === '') {
+    if (data.mbti.trim() === '') {
       return alert('Mbti를 입력해주세요.')
     }
-    if (data.Material.trim() === '') {
+    if (data.material.trim() === '') {
       return alert('재료를 선택해주세요.')
     }
-    if (data.Content.trim() === '') {
+    if (data.content.trim() === '') {
       return alert('레시피를 입력해주세요.')
     }
 
     const formData = new FormData()
-    formData.append('file', isImage.image_file);
-    formData.append('title', data.Title);
-    formData.append('mbti', data.Mbti);
-    formData.append('material', data.Material);
-    formData.append('content', data.Content);
+    formData.append('image', isImage);
+
+    const body = [{
+      title: data.title,
+      mbti: data.mbti,
+      material: data.material,
+      content: data.content,
+      // image: data.imageUrl,
+    }]
+    console.log(body);
+
+    formData.append("data", new Blob([JSON.stringify(body)], {type: "application/json"}))
     
     for (let key of formData.keys()) {
       console.log(key, ":", formData.get(key));
     }
+    console.log('token : ', token)
     
-
     // axios 활용 서버에 전송 하기
-    axios
-    .post(
-      `http://3.36.29.101/api/recipe`,  formData ,
-       {
-        headers: {
-          "Content-Type": "multipart/form-data",
-          "Access-Control-Allow-Origin": "*",
-        },
-      // Image: formData,
-      // Title: data.Title,
-      // Mbti: data.Mbti,
-      // Material: data.Material,
-      // Content: data.Content,
-    })
-    .then((res) => {
-      if (res.data.success) {
-        alert('레시피가 작성되었습니다.');
-        navigate('/detail/:id');
-      } else {
-        alert('작성에 실패하였습니다.');
-        navigate('/createform');
-      }
-    })
-    .catch((err) => {
-      alert('작성에 실패하였습니다.');
-      navigate('/createform');
-    });
+    const res = axios
+      .post(
+        // `http://3.36.29.101/api/recipe`
+        `http://3.36.29.101/api/recipe`, formData
+        // {
+        //   title: data.title,
+        //   mbti: data.mbti,
+        //   material: data.material,
+        //   content: data.content,
+        //   image: data.imageUrl,
+        // }
+        ,
+        {
+          headers: {
+            Authorization: token,
+            contentType : "multipart/form-data"
+          },
+        }
+      )
+      .then((res) => {
+        console.log(res)
+        navigate('/detail/:id')
+        alert('작성완료')
+      })
+      .catch((error) => {
+        console.log(error)
+        alert(error.response.data.message)
+      })
 
     // 데이터 내용 초기화
-    setImage({
-      image_file: "",
-      preview_URL: "",
-    });
+    setImage('');
     setData({
-      Title: '',
-      Mbti: '',
-      Material: '',
-      Content: '',
+      title: '',
+      mbti: '',
+      material: '',
+      content: '',
+      imageUrl: '',
     })
   }
 
@@ -109,80 +115,58 @@ function CreateForm() {
     setTextareaHeight(e.target.value.split('\n').length - 1);
     e.preventDefault();
   };
-
-  // ContentTextarea 컨텐츠 확장기능
-  // 줄 수를 계산해서 저장할 변수
+  // ContentTextarea 컨텐츠 확장기능(줄 수를 계산해서 저장할 변수)
   const [textareaHeight, setTextareaHeight] = useState(0)
 
-  // // 이미지 미리보기 저장할 변수
-  // const [previewImg, setPreviewImg ] = useState<any>([]);
+  
 
-  // 이미지 업로드 input의 onChange
+  // 이미지 업로드 및 미리보기
   const imgHandler = (e : any) => {
     if(e.target.files[0]){
       // 새로운 이미지를 올리면 createObjectURL()을 통해 생성한 기존 URL을 폐기
-      URL.revokeObjectURL(isImage.preview_URL);
-      const preview_URL = URL.createObjectURL(e.target.files[0]);
-      setImage(() => (
-        {
-          image_file: e.target.files[0],
-          preview_URL: preview_URL
-        }
-      ))
+      URL.revokeObjectURL(previewImg);
+      const preview_image = URL.createObjectURL(e.target.files[0]);
+      setImage(e.target.files[0]);
+      setPreviewImg(preview_image);
     }
   };
 
-  // useEffect(()=> {
-  //   // 컴포넌트가 언마운트되면 createObjectURL()을 통해 생성한 기존 URL을 폐기
-  //   return () => {
-  //     URL.revokeObjectURL(isImage.preview_URL)
-  //   }
-  // }, [])
+  useEffect(()=> {
+    // 컴포넌트가 언마운트되면 createObjectURL()을 통해 생성한 기존 URL을 폐기
+    return () => {
+      URL.revokeObjectURL(previewImg)
+    }
+  }, [])
 
-
-  //   const { name, value } = e.target;
-
-  //   // FileReader API로 이미지 인식
-  //   let reader = new FileReader();
-  //   const file = e.target.files[0];
-  //   console.log(e.target.files);
-
-  //   if(file) {
-  //     reader.readAsDataURL(file);  // 1. reader에게 file을 먼저 읽히고
-      
-  //     setData({ ...data, [name]: value});
-  //   }
-
-  //   // 2. 사진 올리고 나서 처리하는 event
-  //   reader.onloadend = () => {
-  //     const previewImgUrl = reader.result || null
-  //     if(previewImgUrl) {
-  //       setPreviewImg([...previewImg, previewImgUrl])
-  //     }
-  //     e.target.value = ''; // 3. 같은 파일을 올리면 인지못해서 여기서 초기화
-  //   }; // 4. 비동기적으로 load가 끝나면 state에 저장
-  // };
-
+  // test
   return (
     <CreateWrapper>
       <Forms onSubmit={onClickHandler}>
         <ImgForm> 
-          <Img  alt="이미지추가" src={isImage.preview_URL} /> 
+          <Img  
+            alt="이미지 들어가는 곳"
+            src={previewImg} 
+            // src={data.imageUrl}
+          /> 
           <ImgInput 
             type="file"
             multiple
-            name="Image"
+            name="image"
             accept="image/*"
             onChange={imgHandler}
             onClick={(e :any) => e.target.value = null}
             ref={refParam => inputRef = refParam}
+
+            // type="text"
+            // name="imageUrl"
+            // onChange={changhandler}
           />
         </ImgForm>
         <CreateFormWrapIn>
           <TitleForm>
             <InputBox
               type="text"
-              name="Title"
+              name="title"
               onChange={changhandler}
               placeholder="제목을 적어주세요!"
             />
@@ -190,12 +174,12 @@ function CreateForm() {
           <MbtiForm>
             <InputBox
               type="text"
-              name="Mbti"
+              name="mbti"
               onChange={changhandler}
               placeholder="mbti를 적어주세요!"
             />
           </MbtiForm>
-          <MaterialForm name="Material" onChange={changhandler}>
+          <MaterialForm name="material" onChange={changhandler}>
             <option value="" hidden>
               주 재료를골라주세요!
             </option>
@@ -208,7 +192,7 @@ function CreateForm() {
         </CreateFormWrapIn>
         <ContentForm>
           <ContentTextarea
-            name="Content"
+            name="content"
             placeholder="당신의 레시피를 적어주세요!"
             onChange={changhandler}
             style={{ height: (textareaHeight + 2) * 18 + 'px' }}
@@ -221,9 +205,9 @@ function CreateForm() {
           <Button
             bgColor="#fff"
             border="3px solid black"
-            onClick={() => {
-              navigate('/mainpage')
-            }}
+            // onClick={() => {
+            //   navigate('/mainpage')
+            // }}
           >
             취소
           </Button>
