c5b978840318d6fa653cacb7580d805a309955c0
Taffymark1 <pjhyun314@gmail.com>
2023-03-02 18:45:46 +0900

    CreataDev 최종본

--- src/page/CreateForm.tsx
+++ src/page/CreateForm.tsx
@@ -1,16 +1,16 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
-import { useDispatch } from 'react-redux'
 import { getCookie } from '../util/cookie'
 import styled from 'styled-components'
+import { title } from 'process'
 
 function CreateForm() {
   const navigate = useNavigate()
-  const dispatch = useDispatch()
+  const fileSizeMax = 3 * 1024 * 1024;  // 3mb 제한
   let inputRef;
   let token = getCookie('accessToken') // 쿠키에저장
-  const [formData, setFormData] = useState<any>(new FormData());
+  const [image, setImage] = useState([]);
   const [previewImg, setPreviewImg ] = useState('');
   const [data, setData] = useState({
     title: '',
@@ -18,19 +18,34 @@ function CreateForm() {
     material: '',
     content: '',
   })
+  const [titleInputCount, setTitleInputCount] = useState(0);
+  const [contentInputCount, setContentInputCount] = useState(0);
 
   const onClickHandler = async (e: any) => {
     e.preventDefault();
-    if (formData === '' ) return alert('이미지를 넣어주세요.');
-    if (data.title.trim() === '') return alert('제목을 입력해주세요.');
-    if (data.mbti.trim() === '') return alert('Mbti를 입력해주세요.');  
+    if (image.length === 0 ) return alert('이미지를 넣어주세요.');
+    if (data.title.trim() === '') {
+      return alert('제목을 입력해주세요.');
+    } else if(data.title.length > 20) {
+      return alert('글자 수 초과!');
+    }
+    if (data.mbti.trim() === '') return alert('MBTI를 선택해주세요.');  
     if (data.material.trim() === '') return alert('재료를 선택해주세요.');
-    if (data.content.trim() === '') return alert('레시피를 입력해주세요.');
-  
+    if (data.content.trim() === '') {
+      return alert('레시피를 입력해주세요.');
+    } else if(data.title.length > 255) {
+      return alert('글자 수 초과!');
+    }
+
+    const formData = new FormData();
+    // formData에 이미지 append
+    for (let i = 0; i < image.length; i++) {
+      formData.append('image', image[i])
+    }
+    // formData에 객체를 JSON문법에 맞게 변형
     const {title, mbti, material, content} = data;
     const body = JSON.stringify({title, mbti, material, content});  
     formData.append('data', new Blob([body], {type: "application/json"}));
-
 
     for (let i of formData.entries()) console.log('key', i);
     
@@ -53,32 +68,10 @@ function CreateForm() {
       alert(error.response.data.message)
     })
   
-
     console.log('target :', res);
-           
-
-    // const res = axios
-    //   .post(
-    //     `http://3.36.29.101/api/recipe`, image ,
-    //     {
-    //       headers: {
-    //         Authorization: token,
-    //         'Content-Type' : "multipart/form-data"
-    //       },
-    //     }
-    //   )
-    //   .then((res) => {
-    //     console.log(res)
-    //     navigate('/detail/:id')
-    //     alert('작성완료')
-    //   })
-    //   .catch((error) => {
-    //     console.log(error)
-    //     alert(error.response.data.message)
-    //   })
-
+          
     // 데이터 내용 초기화
-    setFormData(new FormData());
+    setImage([]);
     setData({
       title: '',
       mbti: '',
@@ -87,31 +80,57 @@ function CreateForm() {
     });
     setPreviewImg('');
   }
-
-  // ContentTextarea 컨텐츠 확장기능(줄 수를 계산해서 저장할 변수)
-  const [textareaHeight, setTextareaHeight] = useState(0)
-
-  // 이벤트 핸들러
-  const changhandler = (e: any) => {
+  // title 핸들러
+  const titleHandler = (e: any) => { 
     const { name, value } = e.target
     setData({ ...data, [name]: value })
-
+    // title input 글자수 표기
+    setTitleInputCount(e.target.value.length);
+  }
+  // selectOption 핸들러
+  const selectHandler = (e: any) => {
+    const { name, value } = e.target
+    setData({ ...data, [name]: value })
+  }
+  // ContentTextarea 컨텐츠 확장기능(줄 수를 계산해서 저장할 변수)
+  const [textareaHeight, setTextareaHeight] = useState(0)
+  // content 핸들러
+  const contentHandler = (e: any) => {
+    const { name, value } = e.target
+    setData({ ...data, [name]: value })
+    // content input 글자수 표기
+    setContentInputCount(e.target.value.length)
     // 엔터('\n') 개수를 세서 textareaHeight에 저장
-    setTextareaHeight(e.target.value.split('\n').length - 1);
-    e.preventDefault();
+    setTextareaHeight(e.target.value.split('\n').length - 1); 
   };
-  
-
-  
-
   // 이미지 업로드 및 미리보기
   const imgHandler = async (e : any) => {
     const {target: {files}} = e;
-    console.log(e.target.files[0])
+    console.log(files[0])
+    // 파일이 없을때 체크
     if(!files[0]) return;
+
+    var reg = /(.*?)\.(jpg|jpeg|png|webp)$/;
     for (let i = 0; i < files.length; i++) {
-      formData.append('image', files[i]);
+      // 파일 확장자 체크
+      if(!files[i].name.match(reg)) {
+        e.target.value = '';
+        setPreviewImg('');
+        alert(`업로드 가능한 확장자가 아닙니다. [가능한 확장자 : jpg jpeg png webp]`)
+        return
+      }
+      // 파일 용량 체크
+      if(files.size > fileSizeMax) {
+        e.target.value = '';
+        setPreviewImg('');
+        alert('업로드 가능한 최대 용량은 3MB입니다. ')
+        return;
+      }
     }
+    
+    // 이미지를 state에 셋
+    setImage(files);
+    // 미리보기용 url
     const objectURL = URL.createObjectURL(e.target.files[0])
     setPreviewImg(objectURL);
   };
@@ -122,8 +141,6 @@ function CreateForm() {
       URL.revokeObjectURL(previewImg)
     }
   }, [])
-
-  // test
   return (
     <CreateWrapper>
       <Forms onSubmit={onClickHandler}>
@@ -136,7 +153,7 @@ function CreateForm() {
             type="file"
             multiple
             name="image"
-            accept="image/*"
+            accept="image/jpg,jpeg,png,webp"
             onChange={imgHandler}
             onClick={(e :any) => e.target.value = null}
             ref={refParam => inputRef = refParam}
@@ -147,36 +164,70 @@ function CreateForm() {
             <InputBox
               type="text"
               name="title"
-              onChange={changhandler}
+              onChange={titleHandler}
+              maxLength={20}
               placeholder="제목을 적어주세요!"
             />
+            <p>
+              <span>{titleInputCount}</span>
+              <span>/20 자</span>
+            </p>
           </TitleForm>
           <MbtiForm>
-            <InputBox
-              type="text"
-              name="mbti"
-              onChange={changhandler}
-              placeholder="mbti를 적어주세요!"
-            />
+            <SelectBox name="mbti" onChange={selectHandler}>
+              <option value="" hidden>
+                MBTI를 골라주세요!
+              </option>
+              <option value="ISTJ">ISTJ</option>
+              <option value="ISTP">ISTP</option>
+              <option value="INFJ">INFJ</option>
+              <option value="INTJ">INTJ</option>
+              <option value="ISFJ">ISFJ</option>
+              <option value="ISFP">ISFP</option>
+              <option value="INFP">INFP</option>
+              <option value="INTP">INTP</option>
+              <option value="ESTJ">ESTJ</option>
+              <option value="ESFP">ESFP</option>
+              <option value="ENFP">ENFP</option>
+              <option value="ENTP">ENTP</option>
+              <option value="ESFJ">ESFJ</option>
+              <option value="ESTP">ESTP</option>
+              <option value="ENFJ">ENFJ</option>
+              <option value="ENTJ">ENTJ</option>
+            </SelectBox>
           </MbtiForm>
-          <MaterialForm name="material" onChange={changhandler}>
-            <option value="" hidden>
-              주 재료를골라주세요!
-            </option>
-            <option value="사케">사케</option>
-            <option value="와인">와인</option>
-            <option value="위스키">위스키</option>
-            <option value="막걸리">막걸리</option>
-            <option value="기타">기타</option>
+          <MaterialForm>
+            <SelectBox name="material" onChange={selectHandler}>
+              <option value="" hidden>
+                주 재료를골라주세요!
+              </option>
+              <option value="사케">사케</option>
+              <option value="맥주">맥주</option>
+              <option value="탁주">탁주</option>
+              <option value="와인">와인</option>
+              <option value="증류주">증류주</option>
+              <option value="막걸리">막걸리</option>
+              <option value="위스키">위스키</option>
+              <option value="브랜디">브랜디</option>
+              <option value="샴페인">샴페인</option>
+              <option value="칵테일">칵테일</option>
+              <option value="전통주">전통주</option>
+              <option value="기타">기타</option>
+            </SelectBox>
           </MaterialForm>
         </CreateFormWrapIn>
         <ContentForm>
           <ContentTextarea
             name="content"
             placeholder="당신의 레시피를 적어주세요!"
-            onChange={changhandler}
+            onChange={contentHandler}
+            maxLength={255}
             style={{ height: (textareaHeight + 2) * 18 + 'px' }}
           />
+          <p>
+            <span>{contentInputCount}</span>
+            <span>/255 자</span>
+          </p>
         </ContentForm>
         <SaveButtonContainer>
           <Button bgColor="#000" border="3px solid #fff" color="white">
@@ -185,9 +236,9 @@ function CreateForm() {
           <Button
             bgColor="#fff"
             border="3px solid black"
-            // onClick={() => {
-            //   navigate('/mainpage')
-            // }}
+            onClick={() => {
+              navigate('/mainpage')
+            }}
           >
             취소
           </Button>
@@ -234,17 +285,26 @@ const CreateFormWrapIn = styled.div`
 `
 const TitleForm = styled.div`
   margin: 20px 0px;
+  p {
+    margin-top: 10px;
+    text-align: right;
+  }
 `
 const MbtiForm = styled.div`
   overflow: hidden;
   margin-bottom: 57px;
-  font-size: 35px;
-  font-weight: 800;
-  color: #222222;
   line-height: 36px;
   word-break: break-all;
   margin: 20px 0px;
 `
+const MaterialForm = styled.div`
+  overflow: hidden;
+  margin-bottom: 57px;
+  line-height: 36px;
+  word-break: break-all;
+  margin: 20px 0px;
+`
+
 const InputBox = styled.input`
   width: 100%;
   border: none;
@@ -253,7 +313,7 @@ const InputBox = styled.input`
   height: 50px;
   font-size: 20px;
 `
-const MaterialForm = styled.select`
+const SelectBox = styled.select`
   width: 100%;
   height: 35px;
   background: white;
@@ -264,8 +324,17 @@ const MaterialForm = styled.select`
   border-bottom: 2px solid #000;
   margin: 20px 0px;
   padding-left: 10px;
-  margin: 20px 5px;
+  option {
+    font-size: 14px;
+    padding: 16px 18px;
+    border: none;
+    transition: background-color 0.2s ease-in;
+    &:hover {
+      background-color: #ddd;
+    }
+  }
 `
+
 const ContentForm = styled.div`
   border-top: 1px solid #e0e0e0;
   padding: 20px 0px;
@@ -273,6 +342,10 @@ const ContentForm = styled.div`
   font-weight: initial;
   display: grid;
   grid-column-end: span 2;
+  p {
+    margin-top: 10px;
+    text-align: right;
+  }
 `
 const ContentTextarea = styled.textarea`
   width: 100%;
